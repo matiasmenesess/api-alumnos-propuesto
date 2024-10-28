87844cddf83fd7ae288e3b009d515b2f428d53aa
matiasmenesess <155995043+matiasmenesess@users.noreply.github.com>
2024-10-28 09:20:19 -0500

    Update ModificarAlumno.py

--- ModificarAlumno.py
+++ ModificarAlumno.py
@@ -4,7 +4,7 @@ def lambda_handler(event, context):
     # Entrada (json)
     tenant_id = event['body']['tenant_id']
     alumno_id = event['body']['alumno_id']
-    alumno_datos = event['alumno_datos']
+    alumno_datos = event['body']['alumno_datos']
     # Proceso
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('t_alumnos')
